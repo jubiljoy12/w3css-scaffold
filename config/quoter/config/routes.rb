26656abc1f949e2588346090fc1f825c3ba83a1c
jubilchittilappilly <jubilchittilappilly@gmail.com>
2018-02-03 22:36:49 -0500

    new styling

--- config/quoter/config/routes.rb
+++ config/quoter/config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  
-  
-  resources :users
   get 'static_pages/home'
-
-  get 'static_pages/help'
-
-  get 'static_pages/about'
-
+  
+  get 'static_pages/hlep'
+  
+  get'static_pages/about'
+  
+  resources :quotes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'users#index'
+
+root 'quotes#index'
 
 end
+
--- config/quoter/quoter/quoter/quoter/db/schema.rb
+++ config/quoter/quoter/quoter/quoter/db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180204025129) do
+ActiveRecord::Schema.define(version: 20180130160424) do
 
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
+  create_table "quotes", force: :cascade do |t|
+    t.string "author"
+    t.string "quotation"
+    t.string "source"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
